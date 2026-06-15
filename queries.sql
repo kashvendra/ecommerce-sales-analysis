ce3a64a09f9fd33ccc4e4e1e6837ec7554b859ce
Kashvendra Kumar <157721077+kashvendra@users.noreply.github.com>
2026-06-15 15:16:11 +0530

    queries.sql

--- queries.sql
+++ queries.sql
@@ -55,3 +55,30 @@ SELECT
 FROM ecommerce_sales
 GROUP BY Month
 ORDER BY Month;
+
+-- Monthly_Revenue
+SELECT
+    Month,
+    Monthly_Revenue,
+    LAG(Monthly_Revenue) OVER (ORDER BY Month) AS Prev_Month_Revenue,
+    ROUND(Monthly_Revenue - LAG(Monthly_Revenue) OVER (ORDER BY Month), 2) AS Growth
+FROM (
+    SELECT
+        SUBSTRING(Order_Date, 4, 2) AS Month,
+        ROUND(SUM(Sales_Amount), 2) AS Monthly_Revenue
+    FROM ecommerce_sales
+    GROUP BY Month
+) t;
+
+--Top Product Per Category using RANK()
+SELECT Category, Product_Name, Revenue
+FROM (
+    SELECT
+        Category,
+        Product_Name,
+        ROUND(SUM(Sales_Amount), 2) AS Revenue,
+        RANK() OVER (PARTITION BY Category ORDER BY SUM(Sales_Amount) DESC) AS rnk
+    FROM ecommerce_sales
+    GROUP BY Category, Product_Name
+) ranked
+WHERE rnk = 1;
